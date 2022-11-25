3ca2916428054504fd2c46349e4787eb7ad14b97
Sophie <kiesouwsophie@gmail.com>
2022-11-25 13:39:04 +0100

    puzzle + gitignore

---
+++ PuzzleGen/newPuzzle.hs
@@ -0,0 +1,24 @@
+newtype PuzzleKnS = Puzzle KnowStruct deriving (Eq, Show)
+
+instance Arbitrary PuzzleKnS where
+  arbitrary = do
+    extraDays <- sublistOf $ map P [15..16] -- FIXME: increase?
+    let days = [P 13, P 14] ++ extraDays
+    extraMonths <- sublistOf $ map P [3..4] -- FIXME: increase?
+    let months = [P 1, P 2] ++ extraMonths
+    let myVocabulary = months ++ days
+    possibilities <- sublistOf [ Conj [PrpF d, PrpF m] | d <-
+    days, m <- months ] -- FIXME not empty!
+    let statelaw = Conj
+                       [ Disj possibilities
+                       , Conj [ Neg $ Conj [PrpF d1, PrpF d2] | d1
+                       <- days, d2 <- days, d1 < d2 ]
+                       , Conj [ Neg $ Conj [PrpF m1, PrpF m2] | m1
+                       <- months, m2 <- months, m1 < m2 ]
+                       ]
+    -- FIXME: do not use random obs!
+    obs <- mapM (\i -> do
+      obsVars <- sublistOf myVocabulary
+      return (i,obsVars)
+      ) ["Albert", "Bernard"]
+    return $ Puzzle $ KnS myVocabulary (boolBddOf statelaw) obs
