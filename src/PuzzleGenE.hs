69e6fc3b59e6e723411cb0364e6b7bd016fb7782
Sophie <kiesouwsophie@gmail.com>
2022-12-08 09:40:29 +0100

    less random + muddy children

--- src/PuzzleGenE.hs
+++ src/PuzzleGenE.hs
@@ -3,24 +3,121 @@ module PuzzleGenE where
 import Test.QuickCheck
 
 import SMCDEL.Language 
-import SMCDEL.Internal.TexDisplay()
-import SMCDEL.Explicit.S5 ( randomPartFor, KripkeModelS5(..) )
-import SMCDEL.Symbolic.S5()
+import SMCDEL.Internal.TexDisplay
+import SMCDEL.Explicit.S5 ( KripkeModelS5(..) )
+-- import Data.List (groupBy, elemIndex)
+-- import Control.Exception.Base (tryJust)
+
 
 
 newtype PuzzleKrMS5 = Puzzle2 KripkeModelS5 deriving (Eq,Ord,Show)
 
 instance Arbitrary PuzzleKrMS5 where
-  arbitrary = do
-    let nonActualWorlds =[0,1,2,3]
-    let worlds = 0 : nonActualWorlds
-    val <- mapM (\w -> do
-      myAssignment <- zip defaultVocabulary <$> infiniteListOf (choose (True,False))
-      return (w,myAssignment)
+  arbitrary = myArbitrary 3
+
+myArbitrary :: Int -> Gen PuzzleKrMS5
+myArbitrary n = do
+    let numDates = if n `elem` [3,5]
+                then n 
+                else 3
+    let numMD = if numDates == 3
+                    then 2
+                    else 3
+
+    allMonths <- shuffle [1..12]    
+    allDays <- shuffle [13..28]
+
+    let months = take numMD $ map P allMonths   
+    let days = take numMD $ map P  allDays
+    let myVocabulary = months ++ days
+    
+    let monthsIdx = [0..numMD-1]
+    let daysIdx = [numMD..(2*numMD)-1]
+
+    -- To do: make this an input for function
+    posDates <- shuffle [(x,y) | x<-monthsIdx, y<-daysIdx]
+    let dates = take numDates posDates
+    -- 
+    -- let datesM = groupBy (\a b -> fst a == fst b) dates
+    -- let datesD = groupBy (\a b -> snd a == snd b) dates
+
+        
+    -- To do: not hardcode this
+    let datesM = if numDates == 3
+                 then[[0,1],[2]]
+                 else [[0,1],[2,3],[4]]
+    let datesD = if numDates == 3
+                 then [[0,2], [1]]
+                 else [[0],[1,2],[3,4]]
+    
+    let worlds = [0..numDates-1]
+
+    val <- mapM (\w -> do 
+      let dateM = fst (dates !! w)
+      let dateD = snd (dates !! w)
+      
+      let holder = repeat False
+      let dateBoolM = replaceNth dateM True holder
+      let dateBoolD = replaceNth dateD True dateBoolM
+      
+      let myAssignment = zip myVocabulary dateBoolD
+      return (w, myAssignment) 
       ) worlds
-    parts <- mapM (\i -> do
-      myPartition <- randomPartFor worlds
-      return (i,myPartition)
-      ) ["Albert", "Bernard"]
+
+    let parts = [("Albert", datesM), ("Bernard", datesD)]
     return $ Puzzle2 $ KrMS5 worlds parts val
-  
+
+
+
+
+run :: Int -> IO ()
+run n = do
+  Puzzle2 k <- (generate (myArbitrary n) :: IO PuzzleKrMS5)
+  print k
+  disp k
+  let newk = k `update` albertDoesNotKnow (vocabOf k)
+
+  print newk
+  disp newk
+
+albertDoesNotKnow :: [Prp] -> Form
+albertDoesNotKnow ps = Conj [ Neg (K "Albert" (PrpF p)) | p <- ps, p > P 12 ]
+
+bernardDoesNotKnow :: [Prp] -> Form
+bernardDoesNotKnow ps = Conj [ Neg (K "Bernard" (PrpF p)) | p <- ps, p < P 13 ]
+
+
+
+replaceNth :: Int -> Bool -> [Bool] -> [Bool]
+replaceNth _ _ [] = []
+replaceNth n newVal (x:xs) =
+  if n == 0 
+  then newVal:xs
+  else x:replaceNth (n-1) newVal xs
+
+-- isTuple :: (a, b) -> Bool
+-- isTuple _ = True
+
+-- dateIdx :: [(Int,Int)] ->   [[(Int,Int)]] -> [[Int]]
+-- dateIdx _ [] = []
+-- dateIdx d (x:xs) = 
+--   if isTuple x
+--   then dateIdx d x : dateIdx d xs 
+--   else elemIndex x d : dateIdx d xs
+ 
+ 
+  -- do 
+  -- result <- tryJust isTuple x
+  -- case result of 
+  --   Left _ -> dateIdx d x : dateIdx d xs
+  --   Right _ -> elemIndex x d : dateIdx d xs
+
+
+
+
+  -- do
+  --   result <- tryJust selectDivByZero (evaluate $ 5 `div` 0)
+  --   case result of
+  --       Left what -> putStrLn $ "Division by " ++ what
+  --       Right val -> putStrLn $ "The answer was: " ++ show val
+
--- src/PuzzleGenS.hs
+++ src/PuzzleGenS.hs
@@ -7,28 +7,23 @@ import SMCDEL.Internal.TexDisplay()
 import SMCDEL.Explicit.S5()
 import SMCDEL.Symbolic.S5
 
-
-
 newtype PuzzleKnS = Puzzle KnowStruct deriving (Eq, Show)
 
 instance Arbitrary PuzzleKnS where
   arbitrary = do
-    -- extraDays <- sublistOf $ map P [15..16] -- FIXME: increase?
-    let days = [P 13, P 14] 
-    -- extraMonths <- sublistOf $ map P [3..4] -- FIXME: increase?
-    let months = [P 1, P 2] 
+    extraDays <- sublistOf $ map P [15..16] -- FIXME: increase?
+    let days = [P 13, P 14] ++ extraDays 
+    extraMonths <- sublistOf $ map P [3..4] -- FIXME: increase?
+    let months = [P 1, P 2] ++ extraMonths
     let myVocabulary = months ++ days
     possibilities <- sublistOf [ Conj [PrpF d, PrpF m] | d <- days, m <- months ] -- FIXME not empty!
     let statelaw = Conj
-                       [ Disj possibilities
-                       , Conj [ Neg $ Conj [PrpF d1, PrpF d2] | d1
-                       <- days, d2 <- days, d1 < d2 ]
-                       , Conj [ Neg $ Conj [PrpF m1, PrpF m2] | m1
-                       <- months, m2 <- months, m1 < m2 ]
-                       ]
-    -- FIXME: do not use random obs!
-    obs <- mapM (\i -> do
-      obsVars <- sublistOf myVocabulary
-      return (i,obsVars)
-      ) ["Albert", "Bernard"]
+                    [ Disj possibilities
+                    , Conj [ Neg $ Conj [PrpF d1, PrpF d2] | d1
+                    <- days, d2 <- days, d1 < d2 ]
+                    , Conj [ Neg $ Conj [PrpF m1, PrpF m2] | m1
+                    <- months, m2 <- months, m1 < m2 ]
+                    ]
+
+    let obs = [("Albert", months), ("Bernard", days)]
     return $ Puzzle $ KnS myVocabulary (boolBddOf statelaw) obs
